2c0ce69160545e4652a4cabcffe66e5728a4f64f
CapsMussa <nzbarec@inbox.ru>
2022-12-08 02:30:54 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FlexBox</title>
+    <link rel="stylesheet" href="head.css">
+</head>
+<body>
+<div><img src="img/1.jpg" class="one"/></div>
+<div class="container">
+    <div class="block">Lorem ipsumdolor sit amet, consectetur adipisicing elit...</div>
+</div>
+<div class="block_right">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error facilis id maxime quae, similique temporibus.
+</div>
+<form action="#">
+    <input type="submit" value="DONATE NOW" class="inp">
+</form>
+
+
+
+<div class="head">
+    <img src="img/logo.png" class="img"/>
+    <a href="/" class="object">Главная</a>
+    <a href="/" class="object">Каталог</a>
+    <a href="#" class="object">Наши работы</a>
+    <a href="#" class="object">Фотогалерея</a>
+    <a href="#" class="object">Кто мы?</a>
+</div>
+</body>
+</html>
