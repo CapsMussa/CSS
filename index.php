a50ef3a9dd8420a9ec9bed1e9a5d273939105cca
CapsMussa <nzbarec@inbox.ru>
2022-12-08 13:29:40 +0500

    First Commit

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>FlexBox</title>
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="head.css">
 </head>
 <body>
-<div><img src="img/1.jpg" class="one"/></div>
+<a name="home"><img src="img/1.jpg" class="one"/></a>
 <div class="container">
     <div class="block">Lorem ipsumdolor sit amet, consectetur adipisicing elit...</div>
 </div>
@@ -20,15 +21,34 @@
     <input type="submit" value="DONATE NOW" class="inp">
 </form>
 
+<a name="catalog" class="catalog">
+<div class="head_bl_cat_l">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit.</div>
+<div class="head_bl_cat_r">Cum repellendus repudiandae sint ut. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, quam? Facere nisi quaerat ratione voluptatem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate magni maiores nisi, sed sunt veniam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id libero optio ratione soluta voluptas? Ducimus perspiciatis quidem quisquam ut veniam!</div>
+</a>
+<form action="#">
+    <input type="submit" value="DONATE" class="donate">
+</form>
+
+<a name="api" class="api_blog">
+<div class="head_api">
+    <div class="head_api_text">LATEST CAMPAIGNS</div>
+</div>
+    <div class="text_api">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi asperiores assumenda at atque aut commodi consequatur cupiditate doloremque eaque earum, eius, error est iste itaque, laudantium libero maiores placeat quasi quidem quis quisquam recusandae sapiente temporibus ullam ut voluptates!</div>
+<div class="text_api">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis dignissimos dolor doloremque dolores dolorum eligendi esse eum illum iste iusto libero minima molestiae numquam porro quasi, quod quos reprehenderit sapiente sequi similique temporibus ullam vel. Adipisci animi dolor eveniet labore laborum maiores natus nemo neque officiis perferendis porro praesentium quos recusandae, repellat similique! Dolore doloremque exercitationem officiis perspiciatis provident!</div>
+</a>
 
 
+
+<a name="copy">
+    <div class="copy_text"><a style="text-decoration: none; color:#fff;" href="https://github.com/CapsMussa/">@CapsMussa->GitHub</a></div>
+</a>
 <div class="head">
     <img src="img/logo.png" class="img"/>
-    <a href="/" class="object">Главная</a>
-    <a href="/" class="object">Каталог</a>
-    <a href="#" class="object">Наши работы</a>
-    <a href="#" class="object">Фотогалерея</a>
-    <a href="#" class="object">Кто мы?</a>
+    <a href="#home" class="object">Home</a>
+    <a href="#catalog" class="object">Catalog</a>
+    <a href="#api" class="object">Donations</a>
+    <a href="#home" class="object">PhotoStart</a>
+    <a href="#copy" class="object">Кто мы?</a>
 </div>
 </body>
 </html>
